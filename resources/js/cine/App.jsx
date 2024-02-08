aaa19038412602aae44fb4194e80cc8a39593324
jrj1987 <jrj1987@gmail.com>
2024-02-08 22:36:38 +0100

    Solución npm run dev

--- resources/js/cine/App.jsx
+++ resources/js/cine/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Cine</h1>
+      <h1>Cine </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
