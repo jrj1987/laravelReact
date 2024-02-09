5798cca4c7965be655e2c67ed22a94dd8aad67a9
jrj1987 <jrj1987@gmail.com>
2024-02-09 10:01:01 +0100

    Instalación librerías react-router-dom y añadir algunas rutas de ejemplo

--- resources/js/restaurante/App.jsx
+++ resources/js/restaurante/App.jsx
@@ -1,38 +1,29 @@
 import { useState, useEffect } from 'react'
-import reactLogo from '../../assets/react.svg'
+import {Routes, Route } from "react-router-dom";
+import Principal from './Principal'
+import Reservas from './Reservas'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    fetch('api/prueba')
+    fetch('/api/prueba')
     .then(respuesta => respuesta.json())
     .then(resultado => console.log(resultado))
     .catch(error => console.log(error))
   
   }, [])
-  
+
+  const path = 'restaurante'  
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Restaurante</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path={path} element={<Principal />}></Route>
+        <Route path={path +'/reservas' } element={<Reservas />}></Route>
+      </Routes>
     </>
   )
 }
